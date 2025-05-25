9bedd92c3f7168f83e12b5669be59e43dec12bdf
Muslihiddin01 <mahmadalievmuslihiddin@gmail.com>
2025-05-25 11:37:17 +0500

    get()

--- api.js
+++ api.js
@@ -1,12 +1,11 @@
-
+import getData from "./dom.js";
 let api = "https://6821ee2fb342dce8004c65e8.mockapi.io/User"
 
-async function get() {
+export default async function get() {
     try {
         let response = await fetch(api)
         let data = await response.json()
-        console.log(data);
-        
+        getData(data)        
     } catch (error) {
         console.error(error);
     }
--- dom.js
+++ dom.js
@@ -1,9 +1,20 @@
-
 let box = document.querySelector('.box')
 
-function getData(data) {
+export default function getData(data) {
     box.innerHTML = ""
     data.forEach(e => {
         let div = document.createElement('div')
+
+        let h1Name = document.createElement('h1')
+        h1Name.innerHTML = e.name 
+
+        let pAge = document.createElement('p')
+        pAge.innerHTML = e.age
+
+        let pStatus = document.createElement('p')
+        pStatus.innerHTML = e.status?"Active":"Inactive"
+
+        div.append(h1Name,pAge,pStatus)
+        box.append(div)
     });   
 }
